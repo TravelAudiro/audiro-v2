58c621975fd67386aab3ca28cbb59b6ac47c09d0
Jeonghee Lee <tpqms316@naver.com>
2024-08-15 13:49:11 +0900

    프로젝트초기, gitignore파일

---
+++ audiro/src/main/java/com/audiro/AudiroApplication.java
@@ -0,0 +1,13 @@
+package com.audiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiroApplication.class, args);
+	}
+
+}
---
+++ audiro/src/test/java/com/audiro/AudiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
